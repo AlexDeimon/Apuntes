a11bd99d6a830d7d0edf27db0bea9576453f7094
Diego1809 <diegoa-sandovalf@unilibre.edu.co>
2023-06-24 16:44:52 -0500

    Types

--- TypeScript/Objetos.ts
+++ TypeScript/Objetos.ts
@@ -20,10 +20,10 @@ const ironMan = createHero('Iron Man', 40)
 
 console.log(ironMan)
 
-//* Template union types
+//* Template types
 //* types como nuevo tipos de datos
 type HexadecimalColor = `#${string}`
-//const color1: HexadecimalColor = '9371ff' //! incorrecto
+//const color1: HexadecimalColor = '9371ff'
 const color2: HexadecimalColor = '#9371ff' //* correcto
 
 //* types como tipos de datos dentro de otros types
@@ -37,7 +37,7 @@ type Hero2 = {
 
 function createHero2(name: string, age: number): Hero2 {
     return {
-        //id: '122', //! incoreccto porque no cumple con el id de tipo HeroId
+        //id: '122',
         name, 
         age
     }
@@ -51,3 +51,88 @@ function createHero3(name: string, age: number): Hero2 {
     }
 }
 
+//* union types
+type HeroPowerScale = 'local' | 'planetario' | 'galactico' | 'universal' | 'multiversal'
+
+let multiTipo: number | string
+
+multiTipo = 3
+// multiTipo = true
+
+type Hero3 = { 
+    readonly id?: HeroId,
+    name: string,
+    age: number
+    powerScale?: HeroPowerScale
+}
+
+function createHero4(name: string, age: number): Hero3 {
+    return {
+        id: '122-943-130-620', //* correcto
+        name, 
+        age
+    }
+}
+
+const doctorStrange = createHero4('Doctor Strange', 40)
+
+doctorStrange.powerScale = "universal"
+
+//* Intersection Types
+//* Extender tipos
+type HeroBasicInfo = {
+    name: string,
+    age: number
+}
+
+type HeroProperties = { 
+    readonly id?: HeroId,
+    isActive?: boolean
+    powerScale?: HeroPowerScale
+}
+
+type Hero4 = HeroBasicInfo & HeroProperties //* type Hero creado a partir de 2 Types
+
+function createHero5(input: HeroBasicInfo): Hero4{
+    const {name, age} = input
+    return{
+        id: crypto.randomUUID(),
+        name,
+        age,
+        isActive: true
+    }
+}
+
+//*Type indexing
+//* acceder a types anidados con [] 
+type HeroProperties2 = {
+    isActive: boolean,
+    address: {
+        planet: string,
+        city: string,
+    }
+}
+
+const addressHero: HeroProperties2['address'] = {
+    planet: 'Earth',
+    city: 'New York'
+}
+
+//* Type from value
+//* crear un typo a apartir de una constante
+const address = {
+    planet: 'Earth',
+    city: 'New York'
+}
+//* typeof en Typescript extrae los tipos de objetos, funciones, etc
+type Address = typeof address
+
+//* type of function return
+function createAddress(){
+    return{
+        planet: 'Earth',
+        city: 'New York'
+    }
+}
+//* permite obtener el type que está retornando una función
+type Address2 = ReturnType<typeof createAddress>
