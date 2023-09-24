c334e137e15fac805f9ab8045491d72a5c80014b
AlexDeimon <diegoa-sandovalf@unilibre.edu.co>
2023-09-24 14:33:57 -0500

    Instalación y compilación

---
+++ TypeScript/Que_es_TS.js
@@ -0,0 +1,9 @@
+//* Ejemplo 1
+var a = 'hola'; //* String
+//*a = true        //* cambiando a boolean sale error
+//* Ejemplo 2:
+function suma(a, b) {
+    return a + b;
+}
+suma(1, 2); //* uso correcto = 3
+//*suma('hola',' mundo') //* uso incorrecto = hola mundo
